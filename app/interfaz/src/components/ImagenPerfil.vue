5a70353080d95e25c08a0b0f85bcf725de2e4cb9
LaDexy <adessiregmolina@gmail.com>
2025-07-01 21:14:29 +0200

    Avance 16

--- app/interfaz/src/components/ImagenPerfil.vue
+++ app/interfaz/src/components/ImagenPerfil.vue
@@ -1,53 +1,29 @@
 <template>
-    <div>
-
- <!--Foto de perfil-->
+  <div>
+    <!--Foto de perfil-->
 
     <div class="Perfil">
-
-     
-      <div class="circulo">
-
-         <div class="cards">
-          <div class="card">
-            <div class="content">
-              <div class="img">
-                <img src="../assets/Usuario.png" alt="">
-            </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
+      <div class="circulo" img src="../assets/Usuario.png" alt=""></div>
     </div>
-
-    </div>
+  </div>
 </template>
 
 <script>
-
-export default{
-
-    name: "ImagenPerfil"
-
-}
-
+export default {
+  name: "ImagenPerfil",
+};
 </script>
 
 <style>
-
 /*Circulo de referencia para foto de perfil*/
 
-.Perfil .circulo .cards .card .img{
-
-    position: absolute;
+.Perfil .circulo {
+  position: absolute;
   width: 100px;
   height: 100px;
   background-color: rgb(199, 127, 199);
   border-radius: 50%;
   left: 700px;
   top: 50px;
-
 }
-
 </style>
--- app/interfaz/src/components/TarjetasPerfiles.vue
+++ app/interfaz/src/components/TarjetasPerfiles.vue
@@ -1,50 +1,19 @@
 <template>
   <div>
-    <div class="Carta">
-    <div class="container">
-      <div class="main-card">
-        <div class="cards">
-          <div class="card">
-            <div class="content">
-              <div class="img">
-                <img src="../assets/Perfil.png" alt="">
-            </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
+      
   </div>
 </template>
 
 <script>
-export default {
-  name: "TarjetasPerfiles",
-};
+export default{
+
+name:"TarjetasPerfiles"
+
+}
 </script>
 
+
+
 <style>
-
-/*Configuracion de cartas*/
-
-.cards .card .content .img{
-
-position: absolute;
-height: 130px;
-width: 130px;
-left: 670px;
-top: 400px;
-
-}
-
-.card .content .img img{
-
-height: 100%;
-width: 100%;
-object-fit: cover;
-border-radius: 50%;
-
-}
 
 </style>
