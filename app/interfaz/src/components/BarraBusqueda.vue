62a1d3f94899c35f45c4a4b6bb01be58ee5b5c3a
LaDexy <adessiregmolina@gmail.com>
2025-07-25 12:49:27 +0200

    Avance 23

--- app/interfaz/src/components/BarraBusqueda.vue
+++ app/interfaz/src/components/BarraBusqueda.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
     <div class="IconoBuscar">
-    <div class="buscar">
-      <input
-        type="text"
-        placeholder="Buscar"
-        required
-        v-model="searchTerm"          @input="emitSearch"           @keyup.enter="emitSearch"     >
+      <div class="buscar">
+        <input
+          type="text"
+          placeholder="Buscar"
+          required
+          v-model="searchTerm"
+          @input="emitSearch"
+          @keyup.enter="emitSearch"
+        />
 
-      <div class="Busqueda" @click="emitSearch"> <i class="fa-solid fa-magnifying-glass icon"></i>
+        <div class="Busqueda" @click="emitSearch">
+          <i class="fa-solid fa-magnifying-glass icon"></i>
+        </div>
       </div>
     </div>
-     </div>
   </div>
 </template>
 
@@ -20,7 +24,7 @@ export default {
   name: "BarraBusqueda",
   data() {
     return {
-      searchTerm: '' // Nuevo: Almacena el término de búsqueda
+      searchTerm: "", // Nuevo: Almacena el término de búsqueda
     };
   },
   methods: {
@@ -29,74 +33,71 @@ export default {
      */
     emitSearch() {
       // Usamos trim() para eliminar espacios en blanco al inicio y al final
-      this.$emit('search', this.searchTerm.trim());
-      console.log('Término de búsqueda emitido desde BarraBusqueda:', this.searchTerm.trim());
-    }
-  }
-}
+      this.$emit("search", this.searchTerm.trim());
+      console.log(
+        "Término de búsqueda emitido desde BarraBusqueda:",
+        this.searchTerm.trim()
+      );
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scope>
 /*Barra de busqueda*/
 
+.IconoBuscar {
 
-.buscar{
+position: relative;
 
-  position: fixed; /* ¡Cambiado de 'relative' a 'fixed' para que no se mueva con el scroll! */
+}
+
+.buscar {
+   /* ¡Cambiado de 'relative' a 'fixed' para que no se mueva con el scroll! */
 
   z-index: 1040; /* Asegura que esté por encima del contenido y debajo del menú */
-  
+
   display: flex; /* Para alinear el input y el botón visualmente */
   align-items: center; /* Centrar verticalmente el input y el botón */
   padding: 5px; /* Pequeño padding alrededor del input y el botón */
-  
+
   /* Eliminamos los márgenes que eran para posicionamiento relativo */
-  margin-left: 0; 
-  margin-top: 0; 
+  margin-left: 0;
+  margin-top: 0;
 }
 
-.buscar input{
-
-width: 0px;
-height: 40px;
-padding: 0 20px;
-font-size: 18px;
-color: #000;
-outline: none;
-border: 1px solid silver;
-border-radius: 30px;
-transition: all 0.6s ease;
-
+.buscar input {
+  width: 0px;
+  height: 40px;
+  padding: 0 20px;
+  font-size: 18px;
+  color: #000;
+  outline: none;
+  border: 1px solid silver;
+  border-radius: 30px;
+  transition: all 0.6s ease;
 }
 
-.Busqueda{
-
-position: absolute;
-top: 0;
-right: 0;
-width: 55px;
-height: 55x;
-line-height: 55px;
-background: #e4a0d5;;
-text-align: center;
-color: white;
-font-size: 20px;
-border-radius: 50%;
-cursor: pointer;
-
+.Busqueda {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 55px;
+  height: 55x;
+  line-height: 55px;
+  background: #e4a0d5;
+  text-align: center;
+  color: white;
+  font-size: 20px;
+  border-radius: 50%;
+  cursor: pointer;
 }
 
-.buscar:hover input{
-
-width: 240px;
-
+.buscar:hover input {
+  width: 240px;
 }
 
-.buscar input:focus{
-
-width: 240px;
-
+.buscar input:focus {
+  width: 240px;
 }
-
 </style>
