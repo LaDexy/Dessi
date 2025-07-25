26ed6420f70396045442b80695fd93677c8cc1ce
LaDexy <adessiregmolina@gmail.com>
2025-07-25 13:23:43 +0200

    Avance 25

--- app/interfaz/src/components/IconoEditar.vue
+++ app/interfaz/src/components/IconoEditar.vue
@@ -1,27 +1,33 @@
 <template>
   <!-- Este div es el contenedor del icono del lápiz -->
-  <!-- Al hacer clic, emitirá un evento 'click' al componente padre -->
+ <div class="Lapiz">
   <div class="edit-icon-button" @click="$emit('click')">
     <i class="fa-solid fa-pencil"></i>
+  </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'IconoEditar',
-  emits: ['click'] // Declara que este componente puede emitir un evento 'click'
+  emits: ['click'] 
 };
 </script>
 
 <style scoped>
-/* Estilos para el icono del lápiz */
+
+.Lapiz{
+
+position: relative;
+top: 10%;
+left: 55%;
+
+}
+
 .edit-icon-button {
-  /* Posición absoluta: esto permite que el componente padre lo posicione */
-  /* Los valores 'bottom' y 'right' serán definidos por el padre */
-  position: absolute; 
-  
-  background-color: #0a0f18; /* Color de fondo del lápiz */
-  color: white; /* Color del icono */
+  position: absolute;
+  background-color: #ffffff; /* Rosa pastel de fondo */
+  color:  hsl(300, 29%, 78%); /* Color vino oscuro para el icono para que contraste */
   border-radius: 50%; /* Forma circular */
   width: 30px; /* Tamaño del círculo del lápiz */
   height: 30px;
@@ -29,13 +35,14 @@ export default {
   justify-content: center;
   align-items: center;
   cursor: pointer;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-  z-index: 10; /* Asegura que esté por encima de otros elementos */
-  transition: background-color 0.2s ease; /* Transición suave al pasar el ratón */
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); /* Sombra más suave */
+  z-index: 10;
+  transition: background-color 0.2s ease, transform 0.2s ease; /* Añadida transición para el transform */
 }
 
 .edit-icon-button:hover {
-  background-color: #2c3e50; /* Color más oscuro al pasar el ratón */
+  background-color: #763879; /* Rosa pastel un poco más oscuro al pasar el ratón */
+  transform: scale(1.05); /* Ligeramente más grande al pasar el ratón para un efecto interactivo */
 }
 
 .edit-icon-button i {
